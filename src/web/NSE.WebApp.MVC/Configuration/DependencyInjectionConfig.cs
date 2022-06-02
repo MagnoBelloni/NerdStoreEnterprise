bfc79333e8acaaad60cbf114a937b726a734f867
Magno Belloni <bellonidesouza6@gmail.com>
2022-06-02 15:04:49 -0300

    Add project files.

---
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.WebApp.MVC.Extensions;
+using NSE.WebApp.MVC.Services;
+using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using System;
+using System.Net.Http;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<HttpClientAuthorizationDelegationHandler>();
+
+            services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+            //services.AddHttpClient<ICatalogoService, CatalogoService>()
+            //.AddHttpMessageHandler<HttpClientAuthorizationDelegationHandler>();
+
+            services.AddHttpClient("Refit", options =>
+            {
+                options.BaseAddress = new System.Uri(configuration.GetSection("CatalogoUrl").Value);
+            })
+            .AddHttpMessageHandler<HttpClientAuthorizationDelegationHandler>()
+            .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>)
+            .AddPolicyHandler(PollyExtensions.EsperarTentar())
+            .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUser, AspNetUser>();
+        }
+    }
+
+    public class PollyExtensions
+    {
+        public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
+        {
+            var retry = HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10),
+                }, onRetry: (outcome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Tentando pela {retryCount} vez!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
+
+            return retry;
+        }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Services/ICatalogoService.cs
@@ -0,0 +1,24 @@
+﻿using NSE.WebApp.MVC.Models;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public interface ICatalogoService
+    {
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+    }
+
+    public interface ICatalogoServiceRefit
+    {
+        [Get("/catalogo/produtos/{id}")]
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+        
+        [Get("/catalogo/produtos/")]
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+    }
+}
