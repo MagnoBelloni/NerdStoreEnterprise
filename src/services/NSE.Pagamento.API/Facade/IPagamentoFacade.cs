9fc99351e58f9f6d79d045086822838ef15c12f5
Magno Belloni <bellonidesouza6@gmail.com>
2022-08-10 18:40:38 -0300

    API de Pagamentos

---
+++ src/services/NSE.Pagamento.API/Facade/IPagamentoFacade.cs
@@ -0,0 +1,12 @@
+﻿using NSE.Pagamentos.API.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Pagamentos.API.Facade
+{
+    public interface IPagamentoFacade
+    {
+        Task<Transacao> AutorizarPagamento(Pagamento pagamento);
+        Task<Transacao> CapturarPagamento(Transacao transacao);
+        Task<Transacao> CancelarAutorizacao(Transacao transacao);
+    }
+}
--- src/services/NSE.Pagamento.API/Startup.cs
+++ src/services/NSE.Pagamento.API/Startup.cs
@@ -1,51 +1,54 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using NSE.Pagamentos.API.Configuration;
+using NSE.WebAPI.Core.Identidade;
 
-namespace NSE.Pagamento.API
+namespace NSE.Pagamentos.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostEnvironment hostEnvironment)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddApiConfiguration(Configuration);
+
+            services.AddJwtConfiguration(Configuration);
+
+            services.AddSwaggerConfiguration();
+
+            services.RegisterServices();
+
+            services.AddMessageBusConfiguration(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseSwaggerConfiguration();
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseApiConfiguration(env);
         }
     }
 }
